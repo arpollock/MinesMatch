8484d6b04acdae67869391577ab552ea36e81ebc
alexrpollock <arpollock@mines.edu>
2019-11-28 21:46:48 -1000

    DB connect back to their settings

--- databse_conn.php
+++ databse_conn.php
@@ -1,14 +1,14 @@
 <?php
 	// Data base connection
-	// $dbhost = "localhost";
-	// $username = "root";
-	// $password = "";
-	// $dbname = "lovepotion";
+	$dbhost = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "lovepotion";
 	// alex Data base connection stuff
-	$dbhost = "127.0.0.1"; 
-    $username = "root";
-	$password = "0000";
-	$dbname = "csci445";
+	// $dbhost = "127.0.0.1"; 
+    // $username = "root";
+	// $password = "0000";
+	// $dbname = "csci445";
 	
 	$conn = mysqli_connect($dbhost, $username, $password, $dbname);
 	
